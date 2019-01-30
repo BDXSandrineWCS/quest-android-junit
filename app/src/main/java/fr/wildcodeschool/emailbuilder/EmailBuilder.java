2c429d57ba4db7432d5560fcaf4bab0553199b33
sandrine <poupardsandrine@gmail.com>
2019-01-30 05:47:49 +0100

    Modifier le fichier EmailBuilder pour que tous les tests soient validés. finished

--- app/src/main/java/fr/wildcodeschool/emailbuilder/EmailBuilder.java
+++ app/src/main/java/fr/wildcodeschool/emailbuilder/EmailBuilder.java
@@ -15,11 +15,12 @@ public class EmailBuilder {
     private static final Pattern EMAIL_PATTERN = Pattern.compile(
             "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
                     "\\@" +
-                    "[a-zA-Z0-9\\-]{1,64}" +
                     "(" +
+                    "[a-zA-Z0-9\\-]{1,64}" +
                     "\\." +
-                    "[a-zA-Z0-9\\-]{0,25}" +
-                    ")+"
+                    ")+" +
+                    "[a-zA-Z0-9\\-]{1,25}"
+
     );
 
     private EmailBuilder() {
